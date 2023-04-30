f29216c6c702d44280859a17fad17a8d4ca1aa23
matthewp-eters <56137347+matthewp-eters@users.noreply.github.com>
2023-04-29 17:06:21 -0700

    Update plot_file_data.m

--- data analysis/plot_file_data.m
+++ data analysis/plot_file_data.m
@@ -1,46 +1,65 @@
-%[time, voltage] = plot_file_data();
-%function [time, voltage] = plot_file_data()
-% This function reads data from a text, csv, or tdms file and plots it
-
-% Prompt user to select a file
-[file_name,file_path] = uigetfile({'*.csv;*.txt;*.tdms', 'Supported Files (*.csv,*.txt,*.tdms)'; '*.*', 'All Files (*.*)'}, 'Select a file');
+function [file_data] = plot_file_data()
+% Prompt user to select files
+[file_names,file_path] = uigetfile({'*.csv;*.txt;*.tdms', 'Supported Files (*.csv,*.txt,*.tdms)'; '*.*', 'All Files (*.*)'}, 'Select files', 'MultiSelect', 'on');
 
 % Check if the user cancelled the dialog
-if isequal(file_name,0) || isequal(file_path,0)
+if isequal(file_names,0) || isequal(file_path,0)
     error('File selection cancelled')
 end
 
-% Determine file type based on file extension
-[~,~,ext] = fileparts(file_name);
-switch lower(ext)
-    case '.csv'
-        data = readmatrix(fullfile(file_path, file_name),'NumHeaderLines',7);
-        time = data(:,1);
-        voltage = data(:,3);
-    case '.txt'
-        data = readmatrix(fullfile(file_path, file_name),'NumHeaderLines',16);
-        voltage = data(:,1);
-        time_step = 0.00001;
-        time = (0:length(voltage)-1)*time_step;
-    case '.tdms'
-         import TDMExcelAddIn.*
-         file = TDMS_readTDMSFile(fullfile(file_path, file_name));
-        x_data = file.Data.MeasuredData(1).Data;
-        y_data = file.Data.MeasuredData(2).Data;
-        time = x_data;
-        voltage = y_data;
-    otherwise
-        error('File type not supported');
+% Convert file_names to cell array if it is a character array
+if ischar(file_names)
+    file_names = {file_names};
 end
 
-% Plot the data
-figure
-plot(time, voltage, "LineWidth",1)
-xt = get(gca, 'XTick');
-set(gca, 'XTick',xt, 'XTickLabel',xt/500000)
-xlabel('Time (s)', 'FontSize',30,'FontWeight','bold')
-ylabel('Voltage (V)', 'FontSize',30,'FontWeight','bold')
-ax = gca; 
-ax.FontSize = 16;
+% Initialize file data array
+num_files = length(file_names);
+file_data = cell(1, num_files);
 
-%end
+% Loop through each file
+for i = 1:num_files
+    % Determine file type based on file extension
+    [~,~,ext] = fileparts(file_names{i});
+    switch lower(ext)
+        case '.csv'
+            data = readmatrix(fullfile(file_path, file_names{i}),'NumHeaderLines',7);
+            time = data(:,1);
+            voltage = data(:,3);
+        case '.txt'
+            data = readmatrix(fullfile(file_path, file_names{i}),'NumHeaderLines',16);
+            voltage = data(:,1);
+            time_step = 0.00001;
+            time = ((0:length(voltage)-1)*time_step)';
+        case '.tdms'
+             import TDMExcelAddIn.*
+             file = TDMS_readTDMSFile(fullfile(file_path, file_names{i}));
+            x_data = file.Data.MeasuredData(1).Data;
+            y_data = file.Data.MeasuredData(2).Data;
+            time = x_data;
+            voltage = y_data;
+        otherwise
+            error('File type not supported');
+    end
+    
+    % Store file data in array
+    file_data{i}.time = time;
+    file_data{i}.voltage = voltage;
+    file_data{i}.fs = 100000;
+    file_data{i}.name = file_names{i};
+
+%     % Plot the data
+%     figure
+%     plot(time, voltage, "LineWidth",1)
+%     hold on;
+%     plot(time, lowpass(voltage,1,file_data{i}.fs))
+%     xt = get(gca, 'XTick');
+%     set(gca, 'XTick',xt, 'XTickLabel',xt/100000)
+%     xlabel('Time (s)', 'FontSize',30,'FontWeight','bold')
+%     ylabel('Voltage (V)', 'FontSize',30,'FontWeight','bold')
+%     ax = gca; 
+%     ax.FontSize = 16;
+%     title(file_names{i})
+%     hold off
+end
+
+end
